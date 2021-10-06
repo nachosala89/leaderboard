02bc4921c19aee734498cbb210e366058f1a9756
nachosala <nachosala89@gmail.com>
2021-10-06 09:10:03 -0300

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ import './style.css';
 
 const players = [
   {
-    name: "Name",
+    name: 'Name',
     score: 100,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 20,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 30,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 45,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 70,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 125,
   },
   {
-    name: "Name",
+    name: 'Name',
     score: 180,
-  }
+  },
 ];
 
 const listContainer = document.querySelector('#list-container');
-players.forEach(player => {
+players.forEach((player) => {
   const li = document.createElement('li');
   const span = document.createElement('span');
   span.textContent = `${player.name}: ${player.score}`;
