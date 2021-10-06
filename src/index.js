d6d77697097552325b63d2f65d06942dcbb0b06d
nachosala <nachosala89@gmail.com>
2021-10-06 08:16:29 -0300

    Add main structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const players = [
+  {
+    name: "Name",
+    score: 100,
+  },
+  {
+    name: "Name",
+    score: 20,
+  },
+  {
+    name: "Name",
+    score: 30,
+  },
+  {
+    name: "Name",
+    score: 45,
+  },
+  {
+    name: "Name",
+    score: 70,
+  },
+  {
+    name: "Name",
+    score: 125,
+  },
+  {
+    name: "Name",
+    score: 180,
+  }
+];
+
+const listContainer = document.querySelector('#list-container');
